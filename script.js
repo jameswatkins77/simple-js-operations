c89393199980eab0affc1c36abaaaf4bd581d48f
Jim Watkins <JimWatkins@Jims-MacBook-Pro.local>
2016-05-27 10:19:50 -0600

    finished all four funcitons

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 module.exports = {
-  stringToArray: function(){
-
+  stringToArray: function(string){
+    var results = string.split(' ');
+    return(results);
   },
-  swapInPlace: function(){
 
+  swapInPlace: function(array, i1, i2){
+    var i3 = array[i1];
+    array.splice(i1,1,array[i2]);
+    array.splice(i2,1,i3);
+    return array;
   },
-  arrayToString: function(){
 
+  arrayToString: function(array){
+    var results = array.join(', ');
+    return results;
   },
-  keys: function(){
 
+  keys: function(object){
+    var results =[];
+    for (var key in object) {
+      results.push(key);
+    }
+    return results;
   }
 };
